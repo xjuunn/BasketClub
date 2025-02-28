bea1f8750cbf79edda9f030bfb88aed07b2ce449
Junhsiun <xjuunn@gmail.com>
2025-02-28 17:28:18 +0800

    init

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <title>BasketClub</title>
+    <NuxtLayout>
+      <NuxtPage></NuxtPage>
+    </NuxtLayout>
+  </div>
+</template>
+<script lang="ts" setup>
+useHead({
+  link:[
+    {rel:'icon',type:'image/png',href:'/icon64.png'}
+  ]
+})
+</script>
